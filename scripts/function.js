bd976a91a80fd4ae028ba3d58755b884f77179b4
zlata <chernigovazlata2017@gmail.com>
2023-08-01 18:46:55 +0300

    Added function.js

---
+++ scripts/function.js
@@ -0,0 +1,17 @@
+export function makeRandomID (length) {
+  let result = '',
+      chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
+
+  for (let i = 0; i < length; i++)
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+
+  return result;
+}
+
+export function getDatesFromText(text){
+  let results = text.match(/[0-9]{1,2}([\-\/ \.])[0-9]{1,2}([\-\/ \.])((19)|(20))[0-9]{2}/g);
+
+  if (results && results.length)
+      return results;
+  return [];
+}
